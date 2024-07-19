fa4bf4c082ed335de13a494f77d13af768783545
tttmb <annop.email@gmail.com>
2024-07-19 17:12:42 +0700

    implement reserveTable function using TDD

--- src/services/reservationService.ts
+++ src/services/reservationService.ts
@@ -1,10 +1,12 @@
+import { Reservation } from "../models/reservation";
+
 class ReservationService {
   private totalTableCount: number = 0;
   private remainingTable: number = 0;
   private isInitialized: boolean = false;
+  private reservationData: { [key: string]: Reservation } = {};
 
-  initializeTables(count: number): string | Error {
-    console.log("initializeTables", this.isInitialized);
+  initializeTable(count: number): string | Error {
     if (this.isInitialized) {
       throw new Error("Tables have already been initialized.");
     }
@@ -12,6 +14,32 @@ class ReservationService {
     this.remainingTable = count;
     this.isInitialized = true;
     return "Tables initialized successfully.";
+  }
+
+  reserveTable(customers: number | null) {
+    if (!this.isInitialized) {
+      throw new Error("Tables have not been initialized.");
+    }
+
+    if (!customers || customers < 1) {
+      throw new Error("Invalid number of customers.");
+    }
+
+    const requiredTables = Math.ceil(customers / 4);
+
+    if (requiredTables > this.remainingTable) {
+      throw new Error("Not enough tables available.");
+    }
+
+    const bookingId = `booking_${Date.now()}`;
+    this.reservationData[bookingId] = { bookingId, tableCount: requiredTables };
+    this.remainingTable -= requiredTables;
+
+    return {
+      bookingId,
+      bookedTableCount: requiredTables,
+      remainingTable: this.remainingTable,
+    };
   }
 }
 
--- src/tests/unit/reservationService.test.ts
+++ src/tests/unit/reservationService.test.ts
@@ -1,14 +1,14 @@
 import reservationService from "../../services/reservationService";
 
 describe("ReservationService", () => {
-  beforeAll(() => {
+  beforeEach(() => {
     reservationService["isInitialized"] = false;
     reservationService["totalTableCount"] = 0;
     reservationService["remainingTable"] = 0;
   });
 
   test("initializeTables should initialize tables", () => {
-    const result = reservationService.initializeTables(10);
+    const result = reservationService.initializeTable(10);
     expect(result).toBe("Tables initialized successfully.");
     expect(reservationService["isInitialized"]).toBe(true);
     expect(reservationService["totalTableCount"]).toBe(10);
@@ -16,8 +16,49 @@ describe("ReservationService", () => {
   });
 
   test("initializeTables should get an error after initialize", () => {
-    expect(() => reservationService.initializeTables(10)).toThrow(
-      "Tables have already been initialized."
+    expect(() => {
+      reservationService.initializeTable(10);
+      reservationService.initializeTable(10);
+    }).toThrow("Tables have already been initialized.");
+  });
+
+  test("reserveTable should get an error 'Tables have not been initialized.'", () => {
+    expect(() => {
+      reservationService.reserveTable(null);
+    }).toThrow("Tables have not been initialized.");
+  });
+
+  test("reserveTable should be called after initialized", () => {
+    reservationService.initializeTable(10);
+
+    expect(() => {
+      reservationService.reserveTable(null);
+    }).not.toThrow("Tables have not been initialized.");
+  });
+
+  test("reserveTable should get 'Invalid number of customers.' error, when invalid input", () => {
+    reservationService.initializeTable(10);
+
+    expect(() => {
+      reservationService.reserveTable(null);
+    }).toThrow("Invalid number of customers.");
+  });
+
+  test("reserveTable should get 'Not enough tables available.' error, when Not enough tables left", () => {
+    reservationService.initializeTable(10);
+
+    expect(() => reservationService.reserveTable(41)).toThrow(
+      "Not enough tables available."
     );
+  });
+
+  test("reserveTable should get 'Invalid number of customers.' when invalid input", () => {
+    reservationService.initializeTable(10);
+
+    expect(reservationService.reserveTable(6)).toEqual({
+      bookingId: expect.any(String),
+      bookedTableCount: 2,
+      remainingTable: 8,
+    });
   });
 });
